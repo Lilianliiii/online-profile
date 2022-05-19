7228660efb4c77f44c30b487c9b502bf6a01a719
Lilian Li <lil712@hotmail.com>
2022-05-19 16:30:13 +0800

    fixed tailwind issues, added font

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,4 +1,12 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+  ],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -6,10 +14,13 @@ module.exports = {
       fontFamily: {
         main: ['Oxygen', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'bbyblue': '#D6E5FA',
+      },
     },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
